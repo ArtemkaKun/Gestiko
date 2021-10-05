9d4ccc289c549eb303b64c1d2c6ef7bfffaeaf68
Artem Yurchenko <zumikq78@gmail.com>
2021-10-05 21:15:00 +0200

    GesturesManager class was implemented

---
+++ Assets/Scripts/GesturesSystem/GesturesManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using GesturesSystem.GestureTypes;
+using UnityEngine;
+
+namespace GesturesSystem
+{
+	public class GesturesManager : MonoBehaviour
+	{
+		private Gesture[] TrainingSet { get; set; }
+		private GesturesDataManager DataManager { get; set; } = new GesturesDataManager();
+
+		private void Awake ()
+		{
+			TrainingSet = LoadTrainingSet();
+		}
+
+		private Gesture[] LoadTrainingSet ()
+		{
+			List<Gesture> gestures = new List<Gesture>();
+			string[] gestureFolders = Directory.GetDirectories("Assets/GesturesData");
+
+			foreach (string folder in gestureFolders)
+			{
+				string[] gestureFiles = Directory.GetFiles(folder, "*.xml");
+
+				foreach (string file in gestureFiles)
+				{
+					gestures.Add(DataManager.ReadGesture(file));
+				}
+			}
+
+			return gestures.ToArray();
+		}
+	}
+}
