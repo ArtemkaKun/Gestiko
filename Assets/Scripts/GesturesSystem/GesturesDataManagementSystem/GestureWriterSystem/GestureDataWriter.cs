88f87126d25cfa628bd6834b851430eb56d6bc63
Artem Yurchenko <zumikq78@gmail.com>
2021-10-09 21:43:52 +0200

    GestureDataWriterConstants class was implemented; GestureDataWriter class was refactored

---
+++ Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GestureWriterSystem/GestureDataWriter.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.IO;
+using Unity.Mathematics;
+
+namespace GesturesSystem
+{
+	public class GestureDataWriter
+	{
+		public void WriteGestureXML (Point[] points, string gestureName)
+		{
+			if (Directory.Exists(GestureDataSystemConstants.GesturesFolderPath) == false)
+			{
+				Directory.CreateDirectory(GestureDataSystemConstants.GesturesFolderPath);
+			}
+
+			using StreamWriter newGestureWriter = new StreamWriter(string.Format(GestureDataWriterConstants.GESTURE_DATA_FILE_NAME_TEMPLATE, GestureDataSystemConstants.GesturesFolderPath, gestureName));
+			newGestureWriter.WriteLine(GestureDataWriterConstants.GESTURE_DATA_FILE_SERVICE_HEADER);
+			newGestureWriter.WriteLine(GestureDataWriterConstants.GESTURE_OPEN_TAG_TEMPLATE, gestureName);
+
+			for (int pointIndex = 0; pointIndex < points.Length; pointIndex++)
+			{
+				Point cachedPoint = points[pointIndex];
+				float2 cachedPointPosition = cachedPoint.Position;
+				newGestureWriter.WriteLine(GestureDataWriterConstants.POINT_TAG_TEMPLATE, cachedPoint.ID.ToString(), cachedPointPosition.x.ToString(CultureInfo.CurrentCulture), cachedPointPosition.y.ToString(CultureInfo.CurrentCulture));
+			}
+
+			newGestureWriter.WriteLine(GestureDataWriterConstants.GESTURE_CLOSE_TAG);
+		}
+	}
+}
---
+++ Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GestureWriterSystem/GestureDataWriterConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace GesturesSystem
+{
+	public static class GestureDataWriterConstants
+	{
+		public const string GESTURE_DATA_FILE_NAME_TEMPLATE = "{0}{1}.xml";
+		public const string GESTURE_DATA_FILE_SERVICE_HEADER = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>";
+		public const string GESTURE_OPEN_TAG_TEMPLATE = "<Gesture Name = \"{0}\">";
+		public const string POINT_TAG_TEMPLATE = "\t<Point ID = \"{0}\" X = \"{1}\" Y = \"{2}\" />";
+		public const string GESTURE_CLOSE_TAG = "</Gesture>";
+	}
+}
