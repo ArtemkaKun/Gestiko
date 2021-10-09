42ef3042a21bda205b18b0b7d0b1c650ad0961ca
Artem Yurchenko <zumikq78@gmail.com>
2021-10-09 22:47:18 +0200

    GestureDataWriter class - files copies were handled; class was refactored

--- Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GestureWriterSystem/GestureDataWriter.cs
+++ Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GestureWriterSystem/GestureDataWriter.cs
@@ -9,8 +9,54 @@ namespace GesturesSystem
 		public void WriteGestureXML (Point[] points, string gestureName)
 		{
 			GesturesDataHelper.MakeSureGesturesDataFolderIsExists();
+			WriteGestureDataToFile(points, gestureName, GetNewGestureDataFilePath(gestureName));
+		}
 
-			using StreamWriter newGestureWriter = new StreamWriter(string.Format(GestureDataWriterConstants.GESTURE_DATA_FILE_NAME_TEMPLATE, GestureDataSystemConstants.GesturesFolderPath, gestureName));
+		private string GetNewGestureDataFilePath (string gestureName)
+		{
+			string expectedNewGestureDataFilePath = CreateGestureDataFilePath(gestureName);
+
+			if (File.Exists(expectedNewGestureDataFilePath) == true)
+			{
+				int fileCopyIndex = GetBiggestFileCopyIndex(gestureName) + 1;
+				expectedNewGestureDataFilePath = CreateGestureDataFilePath($"{gestureName}{fileCopyIndex.ToString()}");
+			}
+
+			return expectedNewGestureDataFilePath;
+		}
+
+		private int GetBiggestFileCopyIndex (string gestureName)
+		{
+			string[] allFilePathsInDataDirectory = Directory.GetFiles(GestureDataSystemConstants.GesturesFolderPath);
+			int biggestFileCopyIndex = 0;
+
+			for (int filePathIndex = 0; filePathIndex < allFilePathsInDataDirectory.Length; filePathIndex++)
+			{
+				string cachedFilePath = allFilePathsInDataDirectory[filePathIndex];
+
+				if (cachedFilePath.Contains(gestureName) == true)
+				{
+					if (int.TryParse(Path.GetFileNameWithoutExtension(cachedFilePath)[^1].ToString(), out int fileIndex) == true)
+					{
+						if (fileIndex > biggestFileCopyIndex)
+						{
+							biggestFileCopyIndex = fileIndex;
+						}
+					}
+				}
+			}
+
+			return biggestFileCopyIndex;
+		}
+
+		private string CreateGestureDataFilePath (string gestureName)
+		{
+			return string.Format(GestureDataWriterConstants.GESTURE_DATA_FILE_NAME_TEMPLATE, GestureDataSystemConstants.GesturesFolderPath, gestureName);
+		}
+
+		private void WriteGestureDataToFile (Point[] points, string gestureName, string expectedNewGestureDataFilePath)
+		{
+			using StreamWriter newGestureWriter = new StreamWriter(expectedNewGestureDataFilePath);
 			newGestureWriter.WriteLine(GestureDataWriterConstants.GESTURE_DATA_FILE_SERVICE_HEADER);
 			newGestureWriter.WriteLine(GestureDataWriterConstants.GESTURE_OPEN_TAG_TEMPLATE, gestureName);
 
