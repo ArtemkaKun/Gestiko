c9e7cb6bfabda071f6b6f513f81475209a84df2c
Artem Yurchenko <zumikq78@gmail.com>
2021-10-09 21:47:02 +0200

    GesturesDataHelper class was implemented

---
+++ Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GesturesDataHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace GesturesSystem
+{
+	public static class GesturesDataHelper
+	{
+		public static bool CheckIfGesturesDataFolderIsExists ()
+		{
+			return Directory.Exists(GestureDataSystemConstants.GesturesFolderPath);
+		}
+
+		public static void MakeSureGesturesDataFolderIsExists ()
+		{
+			if (CheckIfGesturesDataFolderIsExists() == false)
+			{
+				Directory.CreateDirectory(GestureDataSystemConstants.GesturesFolderPath);
+			}
+		}
+	}
+}
