558255569964c92981bb1311f25d206b7c1e60d1
Artem Yurchenko <zumikq78@gmail.com>
2021-10-09 21:22:56 +0200

    GestureDataSystemConstants class was implemented

---
+++ Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GestureDataSystemConstants.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace GesturesSystem
+{
+	public static class GestureDataSystemConstants
+	{
+		public static string GesturesFolderPath { get; } = @$"{Application.persistentDataPath}\{GESTURES_CONTENT_FOLDER_NAME}\";
+
+		private const string GESTURES_CONTENT_FOLDER_NAME = "GesturesData";
+	}
+}
