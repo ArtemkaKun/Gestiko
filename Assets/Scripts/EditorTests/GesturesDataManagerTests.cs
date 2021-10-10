f914da11a78afd27944af0063cdb8f2c32016227
Artem Yurchenko <zumikq78@gmail.com>
2021-10-10 20:17:18 +0200

    GesturesDataManagerTests class - Initialize method was implemented; simple WriteTestToFileAndCheckIfThisFileContainsRightData method was implemetned

--- Assets/Scripts/EditorTests/GesturesDataManagerTests.cs
+++ Assets/Scripts/EditorTests/GesturesDataManagerTests.cs
@@ -1,10 +1,23 @@
+using GesturesSystem;
+using GesturesSystem.GestureTypes;
 using NUnit.Framework;
 
 public class GesturesDataManagerTests
 {
+    private GesturesDataManager DataManager { get; set; }
+    
+    [OneTimeSetUp]
+    public void Initialize ()
+    {
+        DataManager = new GesturesDataManager();
+    }
+    
     [Test]
     public void WriteTestToFileAndCheckIfThisFileContainsRightData ()
     {
-        Assert.Fail("Not implemented");
+        Gesture testGesture = DataManager.ReadGestureOldFormat("Assets/GesturesData/10-stylus-MEDIUM/10-stylus-medium-arrowhead-01.xml");
+        DataManager.WriteGesture(testGesture.RawPointsCollection, testGesture.Name);
+        Gesture rewrittenNewGesture = DataManager.ReadGesture(string.Format(GestureDataWriterConstants.GESTURE_DATA_FILE_NAME_TEMPLATE, GestureDataSystemConstants.GesturesFolderPath, testGesture.Name));
+        CollectionAssert.AreEqual(testGesture.RawPointsCollection, rewrittenNewGesture.RawPointsCollection);
     }
 }
