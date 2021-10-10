f4beb6cee41284245240d8b858f89e72b2dbe70f
Artem Yurchenko <zumikq78@gmail.com>
2021-10-10 20:11:41 +0200

    GestureDataWriterConstants class was refactored with GesstureDataSystemConstants

--- Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GestureWriterSystem/GestureDataWriterConstants.cs
+++ Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GestureWriterSystem/GestureDataWriterConstants.cs
@@ -4,8 +4,8 @@
 	{
 		public const string GESTURE_DATA_FILE_NAME_TEMPLATE = "{0}{1}.xml";
 		public const string GESTURE_DATA_FILE_SERVICE_HEADER = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>";
-		public const string GESTURE_OPEN_TAG_TEMPLATE = "<Gesture Name = \"{0}\">";
-		public const string POINT_TAG_TEMPLATE = "\t<Point ID = \"{0}\" X = \"{1}\" Y = \"{2}\" />";
-		public const string GESTURE_CLOSE_TAG = "</Gesture>";
+		public const string GESTURE_OPEN_TAG_TEMPLATE = "<" + GestureDataSystemConstants.GESTURE_TAG + " " + GestureDataSystemConstants.GESTURE_NAME_PARAMETER + " = \"{0}\">";
+		public const string POINT_TAG_TEMPLATE = "\t<" + GestureDataSystemConstants.POINT_TAG + " " + GestureDataSystemConstants.POINT_ID_PARAMETER + " = \"{0}\" " + GestureDataSystemConstants.POINT_X_COORDINATE_PARAMETER + " = \"{1}\" " + GestureDataSystemConstants.POINT_Y_COORDINATE_PARAMETER + " = \"{2}\" />";
+		public const string GESTURE_CLOSE_TAG = "</" + GestureDataSystemConstants.GESTURE_TAG + ">";
 	}
 }
