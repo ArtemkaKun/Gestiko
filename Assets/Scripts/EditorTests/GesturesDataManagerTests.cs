6a8198f84b15e611bddd69238645039b27069dee
Artem Yurchenko <zumikq78@gmail.com>
2021-10-10 20:42:08 +0200

    GesturesDataManagerTests class - Create3GestureFilesWithSameNameAndCheckFilesCountAndNames test created and passed

--- Assets/Scripts/EditorTests/GesturesDataManagerTests.cs
+++ Assets/Scripts/EditorTests/GesturesDataManagerTests.cs
@@ -23,4 +23,28 @@ public class GesturesDataManagerTests
         File.Delete(rewrittenGestureFilePath);
         CollectionAssert.AreEqual(testGesture.RawPointsCollection, rewrittenNewGesture.RawPointsCollection);
     }
+
+    [Test]
+    public void Create3GestureFilesWithSameNameAndCheckFilesCountAndNames ()
+    {
+        Gesture testGesture = DataManager.ReadGestureOldFormat("Assets/GesturesData/10-stylus-MEDIUM/10-stylus-medium-arrowhead-01.xml");
+
+        for (int fileRepeatIndex = 0; fileRepeatIndex < 3; fileRepeatIndex++)
+        {
+            DataManager.WriteGesture(testGesture.RawPointsCollection, testGesture.Name);
+        }
+
+        for (int fileRepeatIndex = 0; fileRepeatIndex < 3; fileRepeatIndex++)
+        {
+            string currentFilePath = string.Format(GestureDataWriterConstants.GESTURE_DATA_FILE_NAME_TEMPLATE, GestureDataSystemConstants.GesturesFolderPath, $"{testGesture.Name}{(fileRepeatIndex == 0 ? "" : fileRepeatIndex.ToString())}");
+            
+            if (File.Exists(currentFilePath) == false)
+            {
+                Assert.Fail($"File {currentFilePath} expected but not found");
+                return;
+            }
+
+            File.Delete(currentFilePath);
+        }
+    }
 }
