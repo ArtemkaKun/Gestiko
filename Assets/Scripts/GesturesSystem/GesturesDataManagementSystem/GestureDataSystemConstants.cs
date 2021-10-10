1e740f43315f453cd5e0c588c5205b5a7b1a93f8
Artem Yurchenko <zumikq78@gmail.com>
2021-10-10 20:11:07 +0200

    GestureDataSystemConstants class - GestureData file's tags were added

--- Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GestureDataSystemConstants.cs
+++ Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GestureDataSystemConstants.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace GesturesSystem
 {
@@ -7,6 +6,13 @@ namespace GesturesSystem
 	{
 		public static string GesturesFolderPath { get; } = @$"{Application.persistentDataPath}\{GESTURES_CONTENT_FOLDER_NAME}\";
 
+		public const string GESTURE_TAG = "Gesture";
+		public const string GESTURE_NAME_PARAMETER = "Name";
+		public const string POINT_TAG = "Point";
+		public const string POINT_ID_PARAMETER = "ID";
+		public const string POINT_X_COORDINATE_PARAMETER = "X";
+		public const string POINT_Y_COORDINATE_PARAMETER = "Y";
+		
 		private const string GESTURES_CONTENT_FOLDER_NAME = "GesturesData";
 	}
 }
