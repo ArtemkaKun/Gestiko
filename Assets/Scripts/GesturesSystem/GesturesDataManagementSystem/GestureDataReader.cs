b416cd9a594c0428d96449b0e38f30582eea279a
Artem Yurchenko <zumikq78@gmail.com>
2021-10-10 20:14:09 +0200

    GestureDataReader class - ReadOldGesture method was implemented

--- Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GestureDataReader.cs
+++ Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GestureDataReader.cs
@@ -38,5 +38,56 @@ namespace GesturesSystem
 
 			return new Gesture(points.ToArray(), gestureName);
 		}
+
+		public Gesture ReadOldGesture (string fileName)
+		{
+			List<Point> points = new List<Point>();
+			XmlTextReader xmlReader = null;
+			int currentStrokeIndex = -1;
+			string gestureName = "";
+
+			try
+			{
+				xmlReader = new XmlTextReader(File.OpenText(fileName));
+
+				while (xmlReader.Read())
+				{
+					if (xmlReader.NodeType != XmlNodeType.Element)
+					{
+						continue;
+					}
+
+					switch (xmlReader.Name)
+					{
+						case "Gesture":
+							gestureName = xmlReader["Name"];
+
+							if (gestureName != null && gestureName.Contains("~"))
+							{
+								gestureName = gestureName[..gestureName.LastIndexOf('~')];
+							}
+
+							if (gestureName != null && gestureName.Contains("_"))
+							{
+								gestureName = gestureName.Replace('_', ' ');
+							}
+
+							break;
+						case "Stroke":
+							currentStrokeIndex++;
+							break;
+						case "Point":
+							points.Add(new Point(currentStrokeIndex, new float2(float.Parse(xmlReader["X"] ?? string.Empty), float.Parse(xmlReader["Y"] ?? string.Empty))));
+							break;
+					}
+				}
+			}
+			finally
+			{
+				xmlReader?.Close();
+			}
+
+			return new Gesture(points.ToArray(), gestureName);
+		}
 	}
 }
