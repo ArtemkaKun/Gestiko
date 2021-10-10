069d7067613b303f57d7c45197c2b8a6635895f6
Artem Yurchenko <zumikq78@gmail.com>
2021-10-10 20:14:37 +0200

    GestureDataManager class was refactored with GestureDataReader

--- Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GesturesDataManager.cs
+++ Assets/Scripts/GesturesSystem/GesturesDataManagementSystem/GesturesDataManager.cs
@@ -1,64 +1,20 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Xml;
-using GesturesSystem.GestureTypes;
-using Unity.Mathematics;
+﻿using GesturesSystem.GestureTypes;
 
 namespace GesturesSystem
 {
 	public class GesturesDataManager
 	{
 		private GestureDataWriter DataWriter { get; } = new GestureDataWriter();
+		private GestureDataReader DataReader { get; } = new GestureDataReader();
 		
+		public Gesture ReadGestureOldFormat (string fileName)
+		{
+			return DataReader.ReadOldGesture(fileName);
+		}
+
 		public Gesture ReadGesture (string fileName)
 		{
-			List<Point> points = new List<Point>();
-			XmlTextReader xmlReader = null;
-			int currentStrokeIndex = -1;
-			string gestureName = "";
-
-			try
-			{
-				xmlReader = new XmlTextReader(File.OpenText(fileName));
-
-				while (xmlReader.Read())
-				{
-					if (xmlReader.NodeType != XmlNodeType.Element)
-					{
-						continue;
-					}
-
-					switch (xmlReader.Name)
-					{
-						case "Gesture":
-							gestureName = xmlReader["Name"];
-
-							if (gestureName != null && gestureName.Contains("~"))
-							{
-								gestureName = gestureName[..gestureName.LastIndexOf('~')];
-							}
-
-							if (gestureName != null && gestureName.Contains("_"))
-							{
-								gestureName = gestureName.Replace('_', ' ');
-							}
-
-							break;
-						case "Stroke":
-							currentStrokeIndex++;
-							break;
-						case "Point":
-							points.Add(new Point(currentStrokeIndex, new float2(float.Parse(xmlReader["X"] ?? string.Empty), float.Parse(xmlReader["Y"] ?? string.Empty))));
-							break;
-					}
-				}
-			}
-			finally
-			{
-				xmlReader?.Close();
-			}
-
-			return new Gesture(points.ToArray(), gestureName);
+			return DataReader.ReadGesture(fileName);
 		}
 
 		public void WriteGesture (Point[] gesturePointsCollection, string gestureName)
